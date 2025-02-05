c35874a160d6b0c72206622d92b2bfd39e04efae
Victor-als <victor.ccx814@gmail.com>
2025-02-05 17:51:32 -0300

    feat: creating a login screen and removing icons

---
+++ components/login-form.tsx
@@ -0,0 +1,40 @@
+"use client"
+import { Button } from "./ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+
+export default function LoginForm () {
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">
+          Login
+        </CardTitle>
+        <CardDescription>
+          Digite seu email para receber um link de login.
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <form action="">
+         <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input 
+              id="email" 
+              type="email" 
+              name="email" 
+              placeholder="Example@gmail.com" 
+              required
+            />
+          </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+         </div>
+        </form>
+      </CardContent>
+    </Card>
+  )
+}
