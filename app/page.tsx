b89e7b900b209b165f3edbbdb75eacc171fbcb94
Victor-als <victor.ccx814@gmail.com>
2025-02-13 13:59:57 -0300

    feat: checking users already logged in to the new group screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-   <div></div>
-  );
+export default async function Home() {
+  const supabase = await createClient();
+  const { data: authUser } = await supabase.auth.getUser();
+
+  if (authUser?.user) {
+    // Se o usuário estiver autenticado, redireciona para criar um novo grupo
+    redirect("/private/grupos/novo");
+  } else {
+    // Se não estiver autenticado, redireciona para o login
+    redirect("/login");
+  }
+
+  return null;    
 }
